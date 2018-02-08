f564840d74adc09d545a57b63b38556547f2c33a
Paul McLanahan <pmac@mozilla.com>
2018-02-08 12:24:35 -0500

    delete me! add env output for debugging

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+set -ex
+
+echo "######## ALL ENV ##########"
+env
+
+echo "######## cat .env #########"
+cat .env
+
 ./manage.py migrate --noinput
 ./manage.py test --noinput --nologcapture -a '!search_tests' --with-nicedots
